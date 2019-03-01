15274e948a49afb3fc632da4e9e2dbb2498fcb1a
RomanGorenkov <roman.gorenkov.one@gmail.com>
2019-03-02 00:17:12 +0300

    brackets: all test done

--- src/index.js
+++ src/index.js
@@ -1,61 +1,116 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  if(checkLength(str)){
-    return true;
-  } else{
-    return false;
-  }
-
   
-}
+  var brackets = [];
+  var count;
+  var lastBrackets;
 
-function checkLength(str){
-  if(str.length % 2 == 0){
-    return true;
-  } else{
-    return false;
-  }
-}
-
-function sortBrackets(str,bracketsConfig){
-  var stack = new Array(bracketsConfig.length);
-  for (let i = 0; i < stack.length; i++) {
-    stack[i] = [];
-  }
-  for (let i = 0; i < bracketsConfig.length; i++) {
-
-    var elem = bracketsConfig[i][0];
-    for( var j = 0; j< str.length;j++){
-
-      if(str.charAt(j) == bracketsConfig[i][0] || str.charAt(j) == bracketsConfig[i][1]){
-        stack[i].push(str.charAt(j));
-      }
-    }
-    console.log(stack[i]);
-  }
-
-}
-
-function checkCorreckClouse(str, bracketsConfig, position){
-  var endBrackets;
+  leter:
   for(let i = 0;i<str.length;i++ ){
+
+    count = brackets.length;
 
     for (let j = 0; j < bracketsConfig.length; j++) {
 
       if(str.charAt(i) == bracketsConfig[j][0]){
+        
+        if(str.charAt(i) == bracketsConfig[j][0] && str.charAt(i) == bracketsConfig[j][1] && str.charAt(i) != lastBrackets){
 
-        endBrackets = bracketsConfig[j][1];
-        checkCorreckClouse(str.substring(i+1,str.length), bracketsConfig, 0);
+          brackets.push(str.charAt(i));
+          lastBrackets = str.charAt(i);
+          break;
+        } else if(!(str.charAt(i) == bracketsConfig[j][0] && str.charAt(i) == bracketsConfig[j][1])){
 
+          brackets.push(str.charAt(i));
+          break;
+        } 
       }
 
-      if(str.charAt(i) == endBrackets){
-        position  = i;
+      if(str.charAt(i) == bracketsConfig[j][1] && brackets[brackets.length - 1] == bracketsConfig[j][0]){
+
+        if(str.charAt(i) == bracketsConfig[j][1] && str.charAt(i) == bracketsConfig[j][0] && brackets[brackets.length - 1] == bracketsConfig[j][0] && lastBrackets == str.charAt(i)){
+          brackets.pop();
+          lastBrackets = brackets[brackets.length -1];
+          break;
+        } else{
+          brackets.pop();
+          break;
+        }
+        
       }
-      
+    }
+
+    if(count == brackets.length){
+      return false;
     }
   }
+  
+  if(brackets.length == 0){
+    return true;
+  } else{
+    return false;
+  }
+  
 }
 
-checkCorreckClouse('()[]',[['(', ')'], ['[', ']']],0);
 
+
+
+
+
+
+console.log(checkCorreckClouse('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])((([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])))',[['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']]));
+
+function checkCorreckClouse(str, bracketsConfig){
+
+  var brackets = [];
+  var count;
+  var lastBrackets;
+
+  leter:
+  for(let i = 0;i<str.length;i++ ){
+
+    count = brackets.length;
+
+    for (let j = 0; j < bracketsConfig.length; j++) {
+
+      if(str.charAt(i) == bracketsConfig[j][0]){
+        
+        if(str.charAt(i) == bracketsConfig[j][0] && str.charAt(i) == bracketsConfig[j][1] && str.charAt(i) != lastBrackets){
+
+          brackets.push(str.charAt(i));
+          lastBrackets = str.charAt(i);
+          break;
+        } else if(!(str.charAt(i) == bracketsConfig[j][0] && str.charAt(i) == bracketsConfig[j][1])){
+
+          brackets.push(str.charAt(i));
+          break;
+        } 
+      }
+
+      if(str.charAt(i) == bracketsConfig[j][1] && brackets[brackets.length - 1] == bracketsConfig[j][0]){
+
+        if(str.charAt(i) == bracketsConfig[j][1] && str.charAt(i) == bracketsConfig[j][0] && brackets[brackets.length - 1] == bracketsConfig[j][0] && lastBrackets == str.charAt(i)){
+          brackets.pop();
+          lastBrackets = brackets[brackets.length -1];
+          break;
+        } else{
+          brackets.pop();
+          break;
+        }
+        
+      }
+    }
+
+    if(count == brackets.length){
+      return false;
+    }
+  }
+  
+  if(brackets.length == 0){
+    return true;
+  } else{
+    return false;
+  }
+  
+}
